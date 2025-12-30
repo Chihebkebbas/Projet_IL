578dba1bb27f81937b3fac07902aff8f6e390dcb
Chihebkebbas <kebbas.chihebeddine1@gmail.com>
2025-12-30 23:30:12 +0100

    Recherche de video UI

--- TogetherPlay/src/pages/HomePage.jsx
+++ TogetherPlay/src/pages/HomePage.jsx
@@ -4,18 +4,33 @@ import Playlist from "../features/Playlist.jsx";
 import Chat from "../features/Chat.jsx";
 import VideoPlayer from "../features/VideoPlayer.jsx";
 import styles from "./HomePage.module.css";
+import { useState } from "react";
+import SearchResults from "../features/SearchResults.jsx";
 
 export default function HomePage() {
+    const [searchQuery, setSearchQuery] = useState("");
+
+    const handleSearch = (query) => {
+        setSearchQuery(query);
+    };
+
     return (
 
         <div className={styles.pageWrapper}>
 
-            <HeaderPrimary />
+            <HeaderPrimary onSearch={handleSearch} />
 
             <div className={styles.layout}>
                 <main className={styles.main}>
-                    <VideoPlayer />
-                    <SuggestionsGrid />
+                    {/* Si on cherche : on affiche les résultats. Sinon : Video + Suggestions */}
+                    {searchQuery ? (
+                        <SearchResults query={searchQuery} />
+                    ) : (
+                        <>
+                            <VideoPlayer />
+                            <SuggestionsGrid />
+                        </>
+                    )}
                 </main>
 
                 <aside className={styles.aside}>
