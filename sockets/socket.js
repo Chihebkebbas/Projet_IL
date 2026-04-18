e877df761348c7491fddea792d42aa9cb965c157
Lina ALILI <alililina163@gmail.com>
2026-04-18 23:03:11 +0200

    ajout de test room  de synchronisation

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,24 +1,67 @@
-const videoEvents = require("./events/videoEvents");
-const annotationEvents = require("./events/annotationEvents");
-const roomManager = require("./rooms/roomManager");
+const { addUserToRoom, removeUserFromRoom } = require("../rooms/roomManager");
 
-module.exports = function(io) {
-
+module.exports = (io) => {
   io.on("connection", (socket) => {
-
     console.log("User connected:", socket.id);
 
-    socket.on("room:join", ({ roomId, username }) => {
-      roomManager.joinRoom(socket, roomId, username, io);
+    // 🔹 rejoindre un salon
+    socket.on("joinRoom", ({ roomId, username }) => {
+      socket.join(roomId);
+
+      addUserToRoom(roomId, socket.id, username);
+
+      console.log(`${username} joined room ${roomId}`);
+
+      io.to(roomId).emit("userJoined", {
+        userId: socket.id,
+        username,
+      });
     });
 
+    // 🔹 quitter salon
+    socket.on("leaveRoom", ({ roomId }) => {
+      socket.leave(roomId);
+      removeUserFromRoom(roomId, socket.id);
+
+      io.to(roomId).emit("userLeft", socket.id);
+    });
+
+    // 🔹 CHAT
+    socket.on("sendMessage", ({ roomId, message, username }) => {
+      io.to(roomId).emit("receiveMessage", {
+        message,
+        username,
+        time: new Date(),
+      });
+    });
+
+    // 🔹 PLAY VIDEO
+    socket.on("videoPlay", ({ roomId, currentTime }) => {
+      socket.to(roomId).emit("videoPlay", { currentTime });
+    });
+
+    // 🔹 PAUSE VIDEO
+    socket.on("videoPause", ({ roomId, currentTime }) => {
+      socket.to(roomId).emit("videoPause", { currentTime });
+    });
+
+    // 🔹 SEEK VIDEO
+    socket.on("videoSeek", ({ roomId, currentTime }) => {
+      socket.to(roomId).emit("videoSeek", { currentTime });
+    });
+
+    // 🔹 AJOUT ANNOTATION
+    socket.on("addAnnotation", ({ roomId, text, time, username }) => {
+      io.to(roomId).emit("newAnnotation", {
+        text,
+        time,
+        username,
+      });
+    });
+
+    // 🔹 déconnexion
     socket.on("disconnect", () => {
-      roomManager.leaveRoom(socket, io);
+      console.log("User disconnected:", socket.id);
     });
-
-    videoEvents(socket, io);
-    annotationEvents(socket, io);
-
   });
-
 };
