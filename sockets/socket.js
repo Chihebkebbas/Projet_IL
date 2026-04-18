88723c99412738a442d7a3068d5bf1189fe29e7a
Lina ALILI <alililina163@gmail.com>
2026-04-18 23:50:29 +0200

    amélioration des risques de synchroisations des socktes

--- sockets/socket.js
+++ sockets/socket.js
@@ -4,66 +4,98 @@ const {
   getRoomUsers,
 } = require("../rooms/roomManager");
 
+const {
+  setVideoState,
+  getVideoState,
+} = require("../state/videoState");
+
+const {
+  addMessage,
+  getMessages,
+  addAnnotation,
+  getAnnotations,
+} = require("../data/store");
+
 module.exports = (io) => {
   io.on("connection", (socket) => {
     console.log("User connected:", socket.id);
 
     // JOIN ROOM
     socket.on("joinRoom", ({ roomId, username }) => {
-      if (!roomId || !username) return;
-
       socket.join(roomId);
+
       addUserToRoom(roomId, socket.id, username);
 
+      // envoyer utilisateurs
       io.to(roomId).emit("roomUsers", getRoomUsers(roomId));
-    });
 
-    // LEAVE ROOM
-    socket.on("leaveRoom", ({ roomId }) => {
-      socket.leave(roomId);
-      removeUserFromRoom(roomId, socket.id);
+      // envoyer état vidéo actuel
+      socket.emit("videoState", getVideoState(roomId));
 
-      io.to(roomId).emit("roomUsers", getRoomUsers(roomId));
+      // envoyer historique chat
+      socket.emit("chatHistory", getMessages(roomId));
+
+      // envoyer annotations
+      socket.emit("annotations", getAnnotations(roomId));
     });
 
     // CHAT
     socket.on("sendMessage", ({ roomId, message, username }) => {
-      if (!message) return;
-
-      io.to(roomId).emit("receiveMessage", {
+      const msg = {
         message,
         username,
         time: new Date(),
-      });
+      };
+
+      addMessage(roomId, msg);
+
+      io.to(roomId).emit("receiveMessage", msg);
     });
 
-    // VIDEO PLAY
+    // PLAY
     socket.on("videoPlay", ({ roomId, currentTime }) => {
+      setVideoState(roomId, {
+        currentTime,
+        isPlaying: true,
+      });
+
       io.to(roomId).emit("videoPlay", { currentTime });
     });
 
-    // VIDEO PAUSE (FIX BUG 🔥)
+    // PAUSE (FIX PRO 🔥)
     socket.on("videoPause", ({ roomId, currentTime }) => {
+      setVideoState(roomId, {
+        currentTime,
+        isPlaying: false,
+      });
+
       io.to(roomId).emit("videoPause", { currentTime });
     });
 
-    // VIDEO SEEK
+    // SEEK
     socket.on("videoSeek", ({ roomId, currentTime }) => {
+      setVideoState(roomId, {
+        currentTime,
+        isPlaying: false,
+      });
+
       io.to(roomId).emit("videoSeek", { currentTime });
     });
 
     // ANNOTATION
     socket.on("addAnnotation", ({ roomId, text, time, username }) => {
-      if (!text) return;
-
-      io.to(roomId).emit("newAnnotation", {
+      const annotation = {
         text,
         time,
         username,
-      });
+      };
+
+      addAnnotation(roomId, annotation);
+
+      io.to(roomId).emit("newAnnotation", annotation);
     });
 
-    // DISCONNECT
+    // LEAVE
     socket.on("disconnect", () => {
       console.log("User disconnected:", socket.id);
     });
